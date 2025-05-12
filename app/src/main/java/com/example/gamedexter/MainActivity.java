fbd1446c59151da1fa7013bed53d3a7eb70f5e07
AmogusTHO <chamquizgum@gmail.com>
2025-05-13 00:45:43 +0600

    Some UI changes: made it more eye-friendly with a dark theme.

--- app/src/main/java/com/example/gamedexter/MainActivity.java
+++ app/src/main/java/com/example/gamedexter/MainActivity.java
@@ -3,16 +3,33 @@ package com.example.gamedexter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+    EditText edt;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn1);
-        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show());
+        tv = findViewById(R.id.tv1);
+        edt = findViewById(R.id.edt1);
+        String text = edt.getText().toString();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_SHORT).show();
+
+                String text = edt.getText().toString();
+                tv.setText(text);
+            }
+        });
+
     }
 }
